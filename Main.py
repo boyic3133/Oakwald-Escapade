4c165e81a13aabc2e09dbbf4dd8da5fee978490d
boyic3133 <boyic3133@wrdsb.ca>
2019-05-21 13:59:07 -0400

    sowad look at this :D

--- Main.py
+++ Main.py
@@ -12,30 +12,6 @@ pygame.display.set_caption('Oakwald Escapade')
 WHITE = [255, 255, 255]
 BLACK = [0, 0, 0]
 
-close = True
-
-def word(text, size, color, x, y):
-    size = pygame.font.SysFont('Arial', size)
-    word_2 = size.render(text, True, color)
-    end = (word_2, (x, y))
-    return end
-
-
-while close:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_ESCAPE:
-                pygame.quit()
-                quit()
-    screen.fill(WHITE)
-    pygame.display.flip()
-
-
-screen.blit(word('this', 70, BLACK, 0, 0))
-
 #this is map 0 is nothing 1 is wall 2 is spawn
 map1 = [
     '111110000011111',
@@ -105,3 +81,32 @@ map4 = [
     '100000000000001',
     '111110000011111',
 ]
+
+
+close = True
+
+def word(text, size, color, x, y):
+    size = pygame.font.SysFont('Arial', size)
+    word_2 = size.render(text, True, color)
+    end = (word_2, (x, y))
+    return end
+
+
+level = [map1, map2, map3, map4]
+
+
+while close:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                quit()
+    screen.fill(WHITE)
+    pygame.display.flip()
+
+
+screen.blit(word('this', 70, BLACK, 0, 0))
+
